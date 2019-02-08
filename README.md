27e1327173cd56d251bf9779a6ee3e45cd5353a2
Mihai Marvel <costymihai320@gmail.com>
2019-02-08 14:28:23 +0200

    Quiz

--- README.md
+++
@@ -1,2 +0,0 @@
-# Short-Quiz
-Web-Development
--- questions/question1/script.js
+++ questions/question1/script.js
@@ -1,92 +1,98 @@
-var time = 3;
-var t;
+  
+	
+	
+	var time = 5;
+ var t;
 
-function myClock(){
-	var green =  document.getElementsByClassName("green");
-	var timer = document.getElementById("time");
-	var button = document.getElementById("next");
-
-	var inner = timer.innerHTML = time;
-	var expire = document.getElementById("expired");
-    var id = document.getElementById("container-image");
-	time-= 1;
-	t = setTimeout(myClock, 1000)
-	if(time == -1){
-    id.style.pointerEvents = "none";
-	
-	timer.style.display = "none";
-	expire.style.display = "block";
-	button.style.display = "block";	
-	clearTimeout(t)
-	
-   
-    
-    
-	
-	
+function myClock(start, container, button, exp){
+	var id= document.getElementById(start);
+	var id2 =  document.getElementById(container);
+	var id3 = document.getElementById(button);
+	var expired =  document.getElementById(exp);
+	id.innerHTML = time;
+	t = setTimeout(function() {myClock(start, container, button, exp)}, 1000);
+	time-=1;
+	if(time <= 0){
+		clearTimeout(t);
+		id2.style.pointerEvents = "none";
+		id3.classList.toggle("button-green-auto");
+		time = 5;
+		expired.style.display = "block";
 	}
 }
 
+	function totalScore(greenButton){
+	var id = document.getElementById("inner");
+	var green = document.getElementById(greenButton);
+	var id2 = document.getElementById("inner2");	
+	if(green.style.display == "block"){
+	 id.innerHTML = parseInt(id.innerHTML) + 1;
+	} 
+}
 
-function greenButton(principal){
+ 
+			
+
+
+
+function greenButton(principal, button, container){
    
-    var id=   document.getElementById(principal);
-	 var green = id.getElementsByClassName("green")[0];
-	var buttonGreen = document.getElementById("button-green"); 
-      var timer = document.getElementById("time");
-    if(green.style.display ="none"){
-      green.style.display ="block";
-      } if (  green.style.display ="block"){
+ 
+	 var green = document.getElementById(principal)
+	var buttonGreen = document.getElementById(button); 
+	var id= document.getElementById(container); 
+	/*var time = document.getElementById("time");*/
+    var inner = document.getElementById("inner");  
+	  
+   
+      green.style.display = "block";
+       if (  green.style.display = "block"){
+		
+		  clearTimeout(t);
+		  time = 5;
 		 
 		 buttonGreen.classList.toggle("button-green-auto");
          id.style.pointerEvents = "none";
 		
-         clearTimeout(t);
-         timer.style.display ="none";
-      }
+       
+      } 
  }  
  
  
-    function redButton(a, principal, greenId){
+    function redButton(a, principal, greenId, button){
 		 var id = document.getElementById(principal);
 		
         var red = id.getElementsByClassName("red");
 		
         var green = document.getElementById(greenId);
+        var id2 = document.getElementById("inner2");
         
-           var timer = document.getElementById("time");
-          var buttonGreen = document.getElementById("button-green"); 
+          var buttonGreen = document.getElementById(button); 
+		
         for(var i = 0; i < red.length; i++){
            red[i].style.display = "none";
-           
+             clearTimeout(t);
+			time = 5;
+			 
              green.style.display = "block";
 		 buttonGreen.classList.toggle("button-green-auto");
 			id.style.pointerEvents = "none";
-             clearTimeout(t);
-             timer.style.display ="none";
+            
         }
 		
         var ar = [a];
       var b =  red[ar].style.display ="block";
-    
+    if (red[ar].style.display == "block"){
+		 id2.innerHTML = parseInt(id2.innerHTML) + 1;
+	}
 }
 
+ 
 
-function totalScore(){
-	 var inner1 = document.getElementById("inner");
-	
-	 var inner2 = document.getElementById("inner2");
-	 var innerTotal = document.getElementById("innerTotal");
-	 var green =  document.getElementById("green");
-	var green2 =  document.getElementById("green2");
-	 if(green.style.display = "block"){
-		 inner1.innerHTML = 1;
-	 }
-	if(green2.style.display = "block"){
-		 inner2.innerHTML = 2;
-	 }
-	innerTotal.innerHTML =  inner1.innerHTML.length + inner2.innerHTML.length;
-}
+
+
+
+
 
 
 
